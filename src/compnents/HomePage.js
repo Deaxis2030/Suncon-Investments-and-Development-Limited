74be081b6032185e17aa53679a9c881d188e433a
Deaxis2030 <malcolmnoel2030@gmail.com>
2026-06-10 13:41:29 -0400

    Integrate new LeadershipSection into HomePage right after About (matching Design System priority for leadership visibility)

--- src/compnents/HomePage.js
+++ src/compnents/HomePage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HeroSection from './HeroSection';
 import ProjectsSection from './ProjectsSection';
 import AboutSection from './AboutSection';
+import LeadershipSection from './LeadershipSection';
 import TestimonialsSection from './TestimonialsSection';
 import NewsSection from './NewsSection';
 import ContactSection from './ContactSection';
@@ -13,6 +14,7 @@ function HomePage() {
             <HeroSection />
             <ProjectsSection />
             <AboutSection />
+            <LeadershipSection />
             <TestimonialsSection />
             <NewsSection />
             <ContactSection />
